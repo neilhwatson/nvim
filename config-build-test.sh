95f7ab04e4b75e8fdc4fdf4aa52c1c7c95e9b856
Neil H watson <neil@watson-wilson.ca>
2022-06-24 20:52:45 -0400

    rename

---
+++ config-build-test.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+set -e
+
+{
+   # Use dwm float mode or geometry tests will fail
+   # export TEST_MAY_FAIL=Test_geometry
+   cd ~/src/vim
+   git reset --hard HEAD
+   git switch master
+   git reset --hard HEAD
+   git pull
+   git checkout v8.2.5060
+
+   ./configure \
+      --enable-rubyinterp=yes \
+      --enable-python3interp=yes \
+      --with-python3-config-dir=$(python3-config --configdir) \
+      --enable-perlinterp=yes \
+      --enable-gui=gtk3 \
+      --enable-cscope \
+      --prefix=$HOME
+
+   make -j3
+   make test
+   make install
+}
